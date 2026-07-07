540837102fbf34eb0b131ce7b2b955d4f231044c
rriarys <rriarys@gmail.com>
2026-07-07 15:38:24 +0300

    feat(auth): add JWT authentication and test endpoints. Closes #9
    Add JWT authentication and authorization middleware, register JwtBearer package, update HTTP examples, and introduce test controller for simulating and verifying JWT-protected endpoints.

---
+++ ToDoList.Api/Controllers/AuthControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Api.Models;
+using ToDoList.Api.Services;
+
+namespace ToDoList.Api.Controllers;
+
+[ApiController]
+[Route("api/test")]
+public class AuthControllerTests(ITokenService tokenService) : ControllerBase
+{
+    [HttpPost("simulate-auth")]
+    public IActionResult SimulateFlow()
+    {
+        // 1. Create a mock user
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Email = "test@example.com",
+            Name = "Tester"
+        };
+
+        // 2. Generate a token for the mock user
+        var token = tokenService.GenerateToken(user);
+
+        return Ok(new { Token = token, Message = "Token successfully generated" });
+    }
+
+    [Authorize]
+    [HttpGet("protected")]
+    public IActionResult GetProtectedData()
+    {
+        return Ok("Success: You are authorized!");
+    }
+
+    [HttpGet("unauthorized")]
+    public IActionResult GetPublicData()
+    {
+        return Ok("This is a public method, accessible to everyone");
+    }
+}
--- ToDoList.Api/Program.cs
+++ ToDoList.Api/Program.cs
@@ -1,5 +1,8 @@
 using BloggingPlatform.API.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using ToDoList.Api.Data;
 using ToDoList.Api.Data.DataExtensions;
 using ToDoList.Api.Options;
@@ -23,6 +26,24 @@ builder.Services.AddDbContext<ToDoListDbContext>(options =>
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(JwtOptions.SectionName));
 builder.Services.AddScoped<ITokenService, TokenService>();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]!))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 builder.Services.AddScoped<IPasswordService, PasswordService>();
 
 var app = builder.Build();
@@ -31,6 +52,9 @@ if (!app.Environment.IsEnvironment("Testing"))
 {
     app.Services.ApplyMigrations();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
