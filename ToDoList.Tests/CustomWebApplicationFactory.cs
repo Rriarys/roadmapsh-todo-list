99fca48a5970548a484ba4f93e8c481b95400ca2
rriarys <rriarys@gmail.com>
2026-07-15 20:40:03 +0300

    feat(tests): add integration test project and infra, part 1.
    Add ToDoList.Tests project with xUnit and EF Core InMemory setup. Implement CustomWebApplicationFactory for integration testing using an in-memory database and configure test environment. Link test project to main API.

---
+++ ToDoList.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using ToDoList.Api.Data;
+
+namespace ToDoList.Api.Tests;
+
+public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<ToDoListDbContext>));
+            services.RemoveAll(typeof(ToDoListDbContext));
+            services.RemoveAll(typeof(Microsoft.EntityFrameworkCore.Infrastructure.IDbContextOptionsConfiguration<ToDoListDbContext>));
+
+            services.AddDbContext<ToDoListDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("InMemoryTodoTestDb");
+            });
+        });
+    }
+}
